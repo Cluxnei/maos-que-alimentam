f52ff627e7c0330fe5ebd117c81d585aa312928b
Cluxnei <cluxnei31@gmail.com>
2020-05-16 03:07:15 -0300

    login

--- dashboard/app/Http/Controllers/Api/UserController.php
+++ dashboard/app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    final public function login(Request $request): JsonResponse
+    {
+        try {
+            $credentials = $request->only(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'success' => false,
+                    'result' => 'Login inválido',
+                ], Response::HTTP_OK);
+            }
+            $user = Auth::user();
+            $token = $user->createToken('AuthToken');
+            return $this->jsonSuccess(compact('user', 'token'));
+        } catch (Exception $exception) {
+            return $this->jsonError(null, [$exception->getMessage()]);
+        }
+    }
 }
--- dashboard/app/Http/Controllers/Controller.php
+++ dashboard/app/Http/Controllers/Controller.php
@@ -5,9 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param $result
+     * @param $errors
+     * @return JsonResponse
+     */
+    protected function jsonError($result, $errors): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'result' => $result,
+            'errors' => $errors
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * @param $result
+     * @param int $responseCode
+     * @return JsonResponse
+     */
+    protected function jsonSuccess($result, $responseCode = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'result' => $result,
+        ], $responseCode);
+    }
 }
